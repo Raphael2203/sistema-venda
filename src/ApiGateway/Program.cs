ae1264a20e713e3f878c89219f0544f2189b385e
raphael <pererap015@gmail.com>
2025-10-14 20:07:55 -0300

    integrando edits finais e readme

--- src/ApiGateway/Program.cs
+++ src/ApiGateway/Program.cs
@@ -1,32 +1,28 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
-using System.Text;
+using MMLib.SwaggerForOcelot.DependencyInjection;
+using MMLib.SwaggerForOcelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Carrega o ocelot.json
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-.AddJwtBearer("Bearer", options =>
-{
-    options.RequireHttpsMetadata = false;
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = false,
-        ValidateAudience = false,
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("api-gateway-jwt-super-segura-1234567890"))
-    };
-});
-
-builder.Services.AddOcelot();
+// Ocelot + SwaggerForOcelot
+builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
 
 var app = builder.Build();
 
-app.UseAuthentication();
-app.UseAuthorization();
+app.UseRouting();
 
+// Swagger unificado dos serviços downstream
+app.UseSwaggerForOcelotUI(opt =>
+{
+    opt.PathToSwaggerGenerator = "/swagger/docs";
+});
+
+// Roteamento Ocelot
 await app.UseOcelot();
+
 app.Run();
